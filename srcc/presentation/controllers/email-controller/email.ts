3b5a883bd0e0a69c474259ce5d149672c4ab31a8
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-22 21:38:59 -0300

    feat: ensure EmailController returns 400 if no email is provided

---
+++ srcc/presentation/controllers/email-controller/email.ts
@@ -0,0 +1,26 @@
+import { Controller } from "../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
+
+export class EmailController implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
+        try{
+            const requireFields = ['email'];
+            for (const field of requireFields) {
+                if (!httpRequest.body[field]) {
+                    return {
+                        body: new Error(`missing params: ${field}`),
+                        statusCode: 400
+                    };
+                }
+            }
+            return {
+                body: httpRequest.body,
+                statusCode: 200
+            };
+        }catch{
+            return {
+                statusCode: 500
+            }
+        };
+     }
+}
